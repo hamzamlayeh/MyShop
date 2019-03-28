6a9fdf3a106df02b3dfe13d2b2452e11ec039a39
hamzamlayeh <hamzamlayeh@outlook.com>
2019-03-28 10:33:42 +0100

    Initial commit

---
+++ app/src/main/java/com/user/myshop/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.user.myshop;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void inscrire(View view) {
+        startActivity(new Intent(this,Inscription.class));
+    }
+
+    public void Valider(View view) {
+        startActivity(new Intent(this,HomeActivity.class));
+    }
+}
---
+++ app/src/main/java/com/user/myshop/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.user.myshop;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+    public static int SPLASH_TIME_OUT = 4000;
+    ImageView logo ;
+    Animation animFadeIn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        logo = (ImageView) findViewById(R.id.logo);
+        animFadeIn = AnimationUtils.loadAnimation(this, R.anim.translate);
+        logo.setAnimation(animFadeIn);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
