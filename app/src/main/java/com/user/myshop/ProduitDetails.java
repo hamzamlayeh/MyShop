891fa0026b2bd9fecc6589f8c3ca8d9e0275cf93
khaled <khaledhizawi@gmail.com>
2019-03-31 14:07:25 +0100

    modif lyaout

--- app/src/main/java/com/user/myshop/ProduitDetails.java
+++ app/src/main/java/com/user/myshop/ProduitDetails.java
@@ -1,17 +1,46 @@
 package com.user.myshop;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.luseen.luseenbottomnavigation.BottomNavigation.BottomNavigationItem;
+import com.luseen.luseenbottomnavigation.BottomNavigation.BottomNavigationView;
+import com.luseen.luseenbottomnavigation.BottomNavigation.OnBottomNavigationItemClickListener;
 
 public class ProduitDetails extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_produit_details);
+        AddMenu();
     }
 
     public void Ajouter(View view) {
+    }
+
+    private void AddMenu() {
+        BottomNavigationItem bottomNavigationItem = new BottomNavigationItem
+                (getString(R.string.profil), ContextCompat.getColor(this, R.color.colorPrimary), R.drawable.ic_profile);
+        BottomNavigationItem bottomNavigationItem1 = new BottomNavigationItem
+                (getString(R.string.produits), ContextCompat.getColor(this, R.color.colorAccent), R.drawable.ic_allproduit);
+        BottomNavigationItem bottomNavigationItem2 = new BottomNavigationItem
+                (getString(R.string.favori), ContextCompat.getColor(this, R.color.colorAccent), R.drawable.ic_favorite);
+        BottomNavigationItem bottomNavigationItem3 = new BottomNavigationItem
+                (getString(R.string.ajouter_produit), ContextCompat.getColor(this, R.color.colorAccent), R.drawable.ic_addproduit);
+        bottomNavigationView.addTab(bottomNavigationItem);
+        bottomNavigationView.addTab(bottomNavigationItem1);
+        bottomNavigationView.addTab(bottomNavigationItem2);
+        bottomNavigationView.addTab(bottomNavigationItem3);
+        bottomNavigationView.setOnBottomNavigationItemClickListener(new OnBottomNavigationItemClickListener() {
+            @Override
+            public void onNavigationItemClick(int index) {
+                Toast.makeText(getApplicationContext(), "Item " + index + " clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
